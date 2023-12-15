0d0bb27dbcf98f6dc15c04356ce902f64753f368
Rob Frampton <roblframpton@gmail.com>
2023-12-15 15:18:49 +0000

    Added workflow

--- src/test/java/ComponentTests.java
+++ src/test/java/ComponentTests.java
@@ -9,14 +9,14 @@ public class ComponentTests {
     public void WhenCreateQuoteThenHtmlIsCreated() {
         QuoteComponent quote = new QuoteComponent("quote text", "author name");
         String html = quote.render();
-        Assert.assertEquals(html, "<div><span>quote text</span><span>- author name</span></div>");
+        Assert.assertEquals(html, "<div class='quote'><span class='quote-text'>quote text</span><span class='quote-author'>- author name</span></div>");
     }
 
     @Test
     public void WhenCreateQuotesThenHtmlIsCreated() {
-        QuotesComponent quotes = new QuotesComponent(List.of(new QuoteComponent("quote text", "author name")));
+        QuotesComponent quotes = new QuotesComponent(List.of());
         String html = quotes.render();
-        Assert.assertEquals(html, "<h1>Famous Quotes</h1>\n<div>\n<div><span>quote text</span><span>- author name</span></div>\n</div>\n");
+        Assert.assertEquals(html, "<h1>Famous Quotes</h1>\n<div class='quotes'>\n</div>\n");
     }
 
 }
